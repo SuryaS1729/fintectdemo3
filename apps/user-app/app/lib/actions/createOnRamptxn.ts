8b1326747ae9b363968749aa03950052c38828ef
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-16 11:58:20 +0530

    completed onrampinh, without the webhook confirmation process

--- apps/user-app/app/lib/actions/createOnRamptxn.ts
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
+
 export async function createOnRampTransaction( amount: number , provider: string){
     const session =  await getServerSession(authOptions)
     const userId = session.user.id;
@@ -14,7 +15,7 @@ export async function createOnRampTransaction( amount: number , provider: string
     }
     await prisma.onRampTransaction.create({
         data:{
-                userId,
+                userId:Number(userId),
                 amount: amount,
                 status: "Processing",
                 startTime: new Date(),
